169cab8ee5fc0dc7f8105c8336a4bb51be566d0c
DavidMBP <steadypathapp@gmail.com>
2017-04-11 14:03:43 -0500

    Added gulp and npm dev modules

---
+++ gulpfile.js
@@ -0,0 +1,63 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var gzip = require('gulp-gzip');
+var clean = require('gulp-clean');
+var htmlmin = require('gulp-htmlmin');
+var runSequence = require('run-sequence');
+var cleanCSS = require('gulp-clean-css');
+var imageResize = require('gulp-image-resize');
+var rename = require("gulp-rename");
+
+
+
+
+gulp.task('js', function() {
+    return gulp.src('src/**/*.js')
+          .pipe(uglify())
+          .pipe(gulp.dest('dist'));
+});
+
+gulp.task('css', function() {
+  return gulp.src('src/**/*.css')
+        .pipe(cleanCSS())
+        // .pipe(gzip({ append: false }))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('html', function() {
+  return gulp.src('src/**/*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('dist'));
+})
+
+gulp.task('clean', function () {
+    return gulp.src('dist', {read: false})
+          .pipe(clean());
+});
+
+gulp.task('profileimg', function() {
+  return gulp.src("src/img/profilepic.jpg")
+  .pipe(imageResize({
+    width: 70,
+    quality: 0.85,
+    samplingFactor:[2, 2],
+    interlace: true,
+    imageMagick: true
+  }))
+  .pipe(gulp.dest('dist/img'));
+})
+
+gulp.task('png', function () {
+    return gulp.src('src/**/*.png')
+          .pipe(gulp.dest('dist'));
+});
+
+gulp.task('jpg', function () {
+    return gulp.src('src/**/*.jpg')
+          .pipe(gulp.dest('dist'));
+});
+
+gulp.task('build', function(callback) {
+  runSequence('clean', 'html', 'js', 'css', 'png', 'jpg', callback);
+});
